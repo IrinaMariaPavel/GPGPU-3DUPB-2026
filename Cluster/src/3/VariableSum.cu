50c494dee0b1be24c91aee5d9f347661ac0c6996
PetrutMit <petrutmit@gmail.com>
2026-07-28 14:50:29 +0300

    Cluster connectivity timeout

--- Cluster/src/3/VariableSum.cu
+++ Cluster/src/3/VariableSum.cu
@@ -1,8 +1,12 @@
 /* Program to compute an embarasingly parallel problem using CUDA
  * Given a vector of numbers, compute the sum of the squares of the numbers
+ * The correct result of this is SIZE * 4 = 4000000, since we fill the vector with 2s and 2^2 = 4
 */
 
 #include <stdio.h>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
 
 #define threadsPerBlock 256
 #define SIZE 1000000
@@ -19,9 +23,11 @@ __global__ void computeSum(unsigned char *vector, int *sum) {
 
     // TODO: Compute the global thread index for this thread. Use blockIdx, blockDim, and threadIdx. It is a 1D grid of 1D blocks, watch slide if not sure how to do this.
     int tid = 0;
+
     // Each thread computes the square of a single element,
     // then adds it to the running total
     if (tid < SIZE) {
+		// TODO: Read about atomic operations in CUDA.
         atomicAdd(sum, vector[tid] * vector[tid]);
     }
 }
@@ -39,6 +45,8 @@ int main(void) {
     cudaCheckError(err);
 
     cudaMemset(d_sum, 0, sizeof(int));
+	// Fill the vector with 2s. Each thread will compute the square of a single element, so the sum should be 4 * SIZE = 4000000
+	// It is a didactic example, so we are using cudaMemset to fill the vector with 2s.
     cudaMemset(d_vector, 2, SIZE * sizeof(unsigned char));
 
     dim3 blockSize(threadsPerBlock);
@@ -59,8 +67,7 @@ int main(void) {
 
     // TODO: sum is a 32-bit int. Each byte in the vector is at most 255, so
     // each squared term is at most 65025, and with SIZE = 1000000 elements
-    // the total can reach into the tens of billions. That's well past
-    // INT_MAX (about 2.15 billion). Try changing the cudaMemset value for
+    // the total can get big easily and overflow. Try changing the cudaMemset value for
     // d_vector from 2 to 200 and watch what happens to the printed sum.
-    // What type should "sum" (and d_sum) actually be to avoid this?
+    // What type should sum actually be to avoid this?
 }
